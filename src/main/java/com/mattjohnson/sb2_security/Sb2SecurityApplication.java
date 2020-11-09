e4a53abb396ea6c35434f55c8b9e7560abf45505
Matt <mateusz.modx@gmail.com>
2020-11-09 06:28:06 +0100

    first commit

---
+++ src/main/java/com/mattjohnson/sb2_security/Sb2SecurityApplication.java
@@ -0,0 +1,13 @@
+package com.mattjohnson.sb2_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb2SecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb2SecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mattjohnson/sb2_security/Sb2SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mattjohnson.sb2_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb2SecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
